279bfeffb0302f72b648be5110e14d51f68adb97
Jim Arment <jim@jarment.com>
2018-03-13 13:12:41 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,48 +4,58 @@ function myReset() {
   location.reload();
 }
 
-function tenPercent() {
-  document.getElementById('b1').style.background = '#8ED081';
-  let bill = parseInt(document.getElementById('userInput').value);
-  let tipVal = (bill * 10 / 100);
-  let newBill = ((Number(bill) + Number(tipVal))).toFixed(2);
-  document.getElementById('tipOne').innerHTML='Ten Percent is $' + tipVal + '';
-  document.getElementById('result1').innerHTML='The total with tip will be $' + newBill + '';
+function setBtnToActive(btnSelector) {
+  document.getElementById(idSelector).style.background = '#8ED081';
 }
 
+function getBill() {
+  return parseInt(document.getElementById('userInput').value);
+}
+
+function calculateBillWithTip(bill, percent) {
+  return (bill * percent / 100 + bill).toFixed(2);
+}
+
+function getCustomTipPercent() {
+  return parseInt(document.getElementById('customTip').value);
+}
+
+function displayResult(resultSelector, billWithTip) {
+  document.getElementById(resultSelector).innerHTML = 'The total with tip will be $' + billWithTip + '';
+}
+
+function tenPercent() {
+  setBtnToActive('b1')
+  const bill = getBill();
+  const newBill = calculateBillWithTip(bill, 10);
+  displayResult('result1', newBill);
+}
 
 function fifteenPercent() {
-  document.getElementById('b2').style.background = '#8ED081';
-  let bill = parseInt(document.getElementById('userInput').value);
-  let tipVal = (bill * 15 / 100);
-  let newBill = ((Number(bill) + Number(tipVal))).toFixed(2);
-  document.getElementById('tipTwo').innerHTML='Fifteen Percent is $' + tipVal + '';
-  document.getElementById('result2').innerHTML='The total with tip will be $' + newBill + '';
+  setBtnToActive('b2')
+  const bill = getBill();
+  const newBill = calculateBillWithTip(bill, 15);
+  displayResult('result2', newBill);
 }
 
 function twentyPercent() {
-  document.getElementById('b3').style.background = '#8ED081';
-  let bill = parseInt(document.getElementById('userInput').value);
-  let tipVal = (bill * 20 / 100);
-  let newBill = ((Number(bill) + Number(tipVal))).toFixed(2);
-  document.getElementById('tipThree').innerHTML='Twenty Percent is $' + tipVal + '';
-  document.getElementById('result3').innerHTML='The total with tip will be $' + newBill + '';
+  setBtnToActive('b3')
+  const bill = getBill();
+  const newBill = calculateBillWithTip(bill, 20);
+  displayResult('result3', newBill);
 }
 
 function twentyFivePercent() {
-  document.getElementById('b4').style.background = '#8ED081';
-  let bill = parseInt(document.getElementById('userInput').value);
-  let tipVal = (bill * 25 / 100);
-  let newBill = ((Number(bill) + Number(tipVal))).toFixed(2);
-  document.getElementById('tipFour').innerHTML='Twenty Five Percent is $' + tipVal + '';
-  document.getElementById('result4').innerHTML='The total with tip will be $' + newBill + '';
+  setBtnToActive('b4')
+  const bill = getBill();
+  const newBill = calculateBillWithTip(bill, 25);
+  displayResult('result4', newBill);
 }
 
 function anyPercent() {
-  document.getElementById('b5').style.background = '#8ED081';
-  let bill = parseInt(document.getElementById('userInput').value);
-  let tip = parseInt(document.getElementById('customTip').value);
-  let tipVal = (bill * tip / 100);
-  let newBill = ((Number(bill) + Number(tipVal))).toFixed(2);
-  document.getElementById('result5').innerHTML='The total with tip will be $' + newBill + '';
+  setBtnToActive('b1')
+  const bill = getBill();
+  const tip = getCustomTipPercent();
+  const newBill = calculateBillWithTip(bill, tip);
+  displayResult('result5', newBill);
 }
